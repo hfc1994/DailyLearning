25ecfc39e033cf8865b61f4d6d22efc6c3349365
hfc <1013040105@qq.com>
2019-07-16 17:43:46 +0800

    [update]

--- src/main/java/SourceCodeReading/TargetClass.java
+++ src/main/java/SourceCodeReading/TargetClass.java
@@ -34,36 +34,36 @@ public class TargetClass {
     private ForkJoinPool forkJoinPool;
     private FutureTask futureTask;
 
+    private Arrays arrays;  // 工具类，简单浏览了
     private Collections collections;    // 简单看了部分方法
     //-------
     private Queue queue;    // 接口
+    private Deque deque;    // 接口，双端队列
     private BlockingQueue blockingQueue;    // 接口
     private ArrayBlockingQueue arrayBlockingQueue;  // 看了实现逻辑，Itr、Itrs和Spliterator没有看
     private LinkedBlockingQueue linkedBlockingQueue;    // 看了实现逻辑，Itr和Spliterator没有看
     private PriorityQueue priorityQueue;    // 非线程安全，逻辑类似ScheduledThreadPoolExecutor -> DelayedWorkQueue
     private PriorityBlockingQueue priorityBlockingQueue;    // 线程安全，逻辑类似PriorityQueue
-    private ConcurrentLinkedQueue concurrentLinkedQueue;
     //-------
     private Map map;    // 简单看了下
     private HashMap hashMap;    // 红黑树相关的还没细看
     private Hashtable hashtable;    // 简单看了下，没有理由会被用到的类
-    private LinkedHashMap linkedHashMap;
+    private LinkedHashMap linkedHashMap;    // 简单看了下，继承自HashMap
     private ConcurrentHashMap concurrentHashMap;
     private WeakHashMap weakHashMap;
-    private TreeMap treeMap;
-    private SortedMap sortedMap;
+    private TreeMap treeMap;    // 基于红黑树算法的map
+    private SortedMap sortedMap;    // 加锁版的TreeMap
     //-------
-    private List list;
-    private ArrayList arrayList;
-    private LinkedList linkedList;
+    private List list;  // 接口
+    private ArrayList arrayList;    // 简单看了下实现
+    private LinkedList linkedList;  // 简单看了下实现
     //-------
     private Set set;
     private HashSet hashSet;
     private LinkedHashSet linkedHashSet;
-    private SortedSet sortedSet;
-    private TreeSet treeSet;
-    //-------
-    Class clazz;
+    // ------
+    private String string;
+    private Integer integer;
 
     public static void main(String[] args) {
     }
