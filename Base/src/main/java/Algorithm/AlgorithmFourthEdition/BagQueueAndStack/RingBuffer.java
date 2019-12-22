dd1e5a9a859b96aa00f759c14f13027a557afd27
hfc <1013040105@qq.com>
2019-12-22 18:07:32 +0800

    [add]RingBuffer

---
+++ Base/src/main/java/Algorithm/AlgorithmFourthEdition/BagQueueAndStack/RingBuffer.java
@@ -0,0 +1,92 @@
+package Algorithm.AlgorithmFourthEdition.BagQueueAndStack;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by hfc on 2019/12/22.
+ *
+ * 1.3.39 环形缓冲区。环形缓冲区，又称为环形队列，是一种定长N的先进先出
+ * 的数据结构。它在进程间的异步数据传输或记录日志文件时十分有用。当缓冲区
+ * 为空时，消费者会在数据存入缓冲区前等待；当缓冲区满时，生产者会等待将数
+ * 据存入缓冲区。为RingBuffer设计一份API并用（回环）数组将其实现。
+ */
+public class RingBuffer<K> {
+
+    private K[] values;
+    private int size;
+    private int head;
+    private int tail;
+
+    private ReentrantLock lock;
+    private Condition notFull;
+    private Condition notEmpty;
+
+    public RingBuffer() {
+        this(16);
+    }
+
+    @SuppressWarnings("unchecked")
+    public RingBuffer(int capacity) {
+        values = (K[]) new Object[capacity];
+        size = 0;
+        head = tail = 0;
+        lock = new ReentrantLock();
+        notFull = lock.newCondition();
+        notEmpty = lock.newCondition();
+    }
+
+    public void put(K element) throws InterruptedException {
+
+        try {
+            lock.lock();
+
+            while (size == values.length) {
+                // .await()会使线程放弃锁
+                // 当被唤醒后，只有线程获取了锁才能继续下去
+                notFull.await();
+            }
+
+            values[head] = element;
+
+            // 到数组末尾时，再添加数据就回到数组头部了
+            if (head == values.length - 1)
+                head = 0;
+            else
+                head++;
+
+            size++;
+            notEmpty.notify();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public K take() throws InterruptedException {
+        try {
+            lock.lock();
+
+            while (size == 0)
+                notEmpty.await();
+
+            K ret = values[tail];
+            values[tail] = null;
+
+            if (tail == values.length - 1)
+                tail = 0;
+            else
+                tail++;
+
+            size--;
+            notFull.notify();
+
+            return ret;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        // TODO: 2019/12/22 检查和测试代码
+    }
+}
