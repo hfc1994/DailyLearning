8d4e962bbe91f3f5904ffc6fe8aca9a34d35debe
hfc <1013040105@qq.com>
2019-12-25 18:22:15 +0800

    [update]

---
+++ Base/src/main/java/Algorithm/AlgorithmFourthEdition/BagQueueAndStack/Buffer.java
@@ -0,0 +1,20 @@
+package Algorithm.AlgorithmFourthEdition.BagQueueAndStack;
+
+/**
+ * Created by hfc on 2019/12/25.
+ *
+ * 1.3.44 文本编辑器的缓冲区。为文本编辑器的缓冲区设计一个数据类型并实现下表中的API
+ * ---------------------------------------------------------------
+ * public class Buffer
+ * ---------------------------------------------------------------
+ *          Buffer()            创建一块空缓冲区
+ * void     insert(char c)      在光标位置插入字符c
+ * char     delete()            删除并返回光标位置的字符
+ * void     left(int k)         将光标向左移动k个位置
+ * void     right(int k)        将光标向右移动k个位置
+ * int      size()              缓冲区中的字符数量
+ * ---------------------------------------------------------------
+ * 提示：使用两个栈。
+ */
+public class Buffer {
+}
